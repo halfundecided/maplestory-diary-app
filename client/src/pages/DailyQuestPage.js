b26f3a158d94516e148a792268d38ffb6773e241
Mijeong Ban <mban1@stevens.edu>
2020-07-10 22:55:12 +0900

    daily quest symbol card

--- client/src/pages/DailyQuestPage.js
+++ client/src/pages/DailyQuestPage.js
@@ -14,8 +14,55 @@ import {
   Thumbnail,
 } from 'native-base';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import symbol from '../assets/images/vj-symbol.png';
+import symbol1 from '../assets/images/symbol-vj.png';
+import symbol2 from '../assets/images/symbol-chuchu.png';
+import symbol3 from '../assets/images/symbol-lachelein.png';
+import symbol4 from '../assets/images/symbol-arcana.png';
+import symbol5 from '../assets/images/symbol-morass.png';
+import symbol6 from '../assets/images/symbol-esfera.png';
 
+const arcaneSymbol = [
+  {
+    title: '소멸의 여로',
+    img: symbol1,
+  },
+  {
+    title: '츄츄 아일랜드',
+    img: symbol2,
+  },
+  {
+    title: '레헬른',
+    img: symbol3,
+  },
+  {
+    title: '아르카나',
+    img: symbol4,
+  },
+  {
+    title: '모라스',
+    img: symbol5,
+  },
+  {
+    title: '에스페라',
+    img: symbol6,
+  },
+  {
+    title: '에르다 스펙트럼',
+    img: symbol1,
+  },
+  {
+    title: '배고픈 무토',
+    img: symbol2,
+  },
+  {
+    title: '드림 브레이커',
+    img: symbol3,
+  },
+  {
+    title: '스피릿 세이비어',
+    img: symbol4,
+  },
+];
 const DailyQuestPage = () => {
   return (
     <Container>
@@ -31,20 +78,27 @@ const DailyQuestPage = () => {
           </Button>
         </Right>
       </Header>
-      <Card>
+      <Card style={{ marginLeft: 5, marginRight: 5 }}>
         <CardItem header bordered>
-          <Text>일일퀘스트 - 심볼</Text>
+          <Text>일일퀘스트 - 아케인 심볼</Text>
         </CardItem>
-        <CardItem bordered>
-          <Thumbnail
-            style={{ marginLeft: 30, marginRight: 10 }}
-            source={symbol}
-          />
-          <Text>소멸의 여로</Text>
-          <Right>
-            <Ionicons name={'checkmark-circle-outline'} size={20} />
-          </Right>
-        </CardItem>
+        {arcaneSymbol.map((symbol) => (
+          <CardItem bordered style={{ height: 55 }}>
+            <Thumbnail
+              style={{
+                marginLeft: 30,
+                marginRight: 10,
+                width: 40,
+                height: 40,
+              }}
+              source={symbol.img}
+            />
+            <Text style={{ flex: 1 }}>{symbol.title}</Text>
+            <Right style={{ marginRight: 30 }}>
+              <Ionicons name={'checkmark-circle-outline'} size={20} />
+            </Right>
+          </CardItem>
+        ))}
       </Card>
     </Container>
   );
